ea18b796f6c35a1afa272d9b76dd4c93621c58c1
Anton Homeniuk <anton.homeniuk@gmail.com>
2021-04-06 16:44:05 +0300

    Update sliver_fill_remaining_box_adapter.dart
    fixed update

--- lib/sliver_fill_remaining_box_adapter.dart
+++ lib/sliver_fill_remaining_box_adapter.dart
@@ -13,7 +13,7 @@ class SliverFillRemainingBoxAdapter extends SingleChildRenderObjectWidget {
 
   @override
   RenderSliverFillRemainingBoxAdapter createRenderObject(BuildContext context) {
-    var scrollable = context.ancestorStateOfType(TypeMatcher<ScrollableState>()) as ScrollableState;
+    var scrollable = context.findAncestorStateOfType<ScrollableState>();
     return RenderSliverFillRemainingBoxAdapter(scrollable.widget.controller);
   }
 }
